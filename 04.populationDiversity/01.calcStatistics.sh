7a2094902ee582afee0186b4e2fbcd800cc895f3
Kenneth C.Y. Wong <1155117922@link.cuhk.edu.hk>
2020-04-11 17:04:20 +0800

    Add command line argument

--- 04.populationDiversity/01.calcStatistics.sh
+++ 04.populationDiversity/01.calcStatistics.sh
@@ -5,9 +5,19 @@ set -e
 BINPATH="/mnt/ws/Resequencing/00.bin"
 DATAPATH="/mnt/ws/hw3/03.variationCalling"
 
+WINDOW_SIZE=$1
+
+if [ "$#" -lt 1 ];
+then
+	echo "Usage: $0 <window size in bp>"
+	exit 0
+fi
+
+WINDOW_SIZE=$1
+
 #Use vcftools to calculate population statistics
 #calculate pi; remember to specify your own .vcf.gz file for this calculation
-$BINPATH/vcftools_0.1.13/bin/vcftools --gzvcf $DATAPATH/filtered.raw.vcf.gz --window-pi 300 --out diversity_level
+$BINPATH/vcftools_0.1.13/bin/vcftools --gzvcf $DATAPATH/filtered.raw.vcf.gz --window-pi $WINDOW_SIZE --out diversity_level
 
 python plotDiversityStatistics.py #plot the diversity level; this is a short script developed by my self, so modify it or use other ways to plot the diversity
 
